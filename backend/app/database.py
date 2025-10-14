0cfa1681126a4248de83a607144888cc5487937d
고태경 <202244042@itc.ac.kr>
2025-10-15 03:19:21 +0900

    save

--- backend/app/database.py
+++ backend/app/database.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv() # Load environment variables from .env file
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL") or os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
